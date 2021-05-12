26fdf10440cf84b64b250fed6486c5845527fff8
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-05-12 19:34:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const age = 18;
+if (age >= 18){
+    console.log('Je mag naar binnen')
+}
+  if (age >=18 && age <= 25){
+    console.log('Je krijgt 50% korting!')
+    
+} else{
+    console.log('Mag Niet naar binnen')
+}
+
+const isFemale = true;
+if (isFemale){
+    console.log('Ladys night')
+
+} else{
+    console.log('Not your night')
+}
+
+const driverStatus = 'bob'
+if (driverStatus=='bob'){
+    console.log('Mag rijden')
+} else{
+    console.log('Mag niet rijden')
+}
+
+const firstName = ['Sarah', 'Randy'];
+    if (firstName == 'Sara' || firstName==' Randy');{
+        console.log('Vandaag krijg je een gratis biertje')
+    }
+
+const totalAmount = [25, 50, 100];
+   if (totalAmount > 25){
+       console.log(' Je krijgt gratis bitterballen!')
+   }  else if (totalAmount > 50){
+       console.log('Je krijgt gratis nachos!')
+       
+   } else if(totalAmount > 100){
+       console.log('Je krigt gratis fles Champange')
+   }
